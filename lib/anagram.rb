0b9a74e3377e602942d32e3ca344a851565020ee
Motti789 <mchillywilly@gmail.com>
2021-07-14 16:38:19 -0400

    commit tree a0b9951beb6ddb81d925e0ab764567fa0b5a7660

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,15 @@
+require 'pry'
 class Anagram
     attr_accessor :some_word
 
-    def initialze
+    def initialize(name)
+        @some_word = name
     end   
 
-    def match
-        @some_word = some_word
-        someword.detect? do |word|
-        end     
+    def match(array_of_words)
+        array_of_words.select {|word|  word.split("").sort == @some_word.split("").sort}
+        #array_of_words.select do |word|   
+        #end
+       
     end
 end           
