d4f8aa0ad7a7547574b7d54d3d7d1cc2e0aca8c0
Motti789 <mchillywilly@gmail.com>
2021-07-14 15:23:54 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :some_word
+
+    def initialze
+    end   
+
+    def match
+        @some_word = some_word
+        someword.detect? do |word|
+        end     
+    end
+end           
